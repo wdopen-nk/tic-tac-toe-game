ee48b6047011be16d710f2442f7d55214b694750
wdopen-nk <nihatsadigzada06@gmail.com>
2025-09-03 13:25:57 +0400

    Add GUI components & configure the constants

--- src/App.java
+++ src/App.java
@@ -1,3 +1,12 @@
-public class App {
+import javax.swing.*;
 
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TicTacToeGUI().setVisible(true);
+            }
+        });
+    }
 }
--- src/TicTacToeGUI.java
+++ src/TicTacToeGUI.java
@@ -1,7 +1,99 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TicTacToeGUI extends JFrame {
+
+    private JLabel turnLabel, scoreLabel;
+    private JButton[][] board;
+
     public TicTacToeGUI() {
         super("Tic Tac Toe");
+        setSize(CommonConstants.FRAME_SIZE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        getContentPane().setBackground(CommonConstants.BACKGROUND_COLOR);
+
+        // initialize variables
+        board = new JButton[3][3];
+
+        addGuiComponent();
+    }
+
+    private void addGuiComponent() {
+        // bar label
+        JLabel barLabel = new JLabel();
+        barLabel.setOpaque(true);
+        barLabel.setBackground(CommonConstants.BAR_COLOR);
+        barLabel.setBounds(0, 0, CommonConstants.FRAME_SIZE.width, 25);
+
+        // turn label
+        turnLabel = new JLabel(CommonConstants.X_LABEL);
+        turnLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        turnLabel.setFont(new Font("Dialog", Font.PLAIN, 40));
+        turnLabel.setPreferredSize(new Dimension(100, turnLabel.getPreferredSize().height));
+        turnLabel.setOpaque(true);
+        turnLabel.setBackground(CommonConstants.X_COLOR);
+        turnLabel.setForeground(CommonConstants.BOARD_COLOR);
+        turnLabel.setBounds(
+                (CommonConstants.FRAME_SIZE.width - turnLabel.getPreferredSize().width)/2,
+                0,
+                turnLabel.getPreferredSize().width,
+                turnLabel.getPreferredSize().height
+        );
+
+        // score label
+        scoreLabel = new JLabel(CommonConstants.SCORE_LABEL);
+        scoreLabel.setFont(new Font("Dialog", Font.PLAIN, 40));
+        scoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        scoreLabel.setForeground(CommonConstants.BOARD_COLOR);
+        scoreLabel.setBounds(
+                0,
+                turnLabel.getY() + turnLabel.getPreferredSize().height + 25,
+                CommonConstants.FRAME_SIZE.width,
+                scoreLabel.getPreferredSize().height
+        );
+
+        // game board
+        GridLayout gridLayout = new GridLayout(3, 3);
+        JPanel boardPanel = new JPanel(gridLayout);
+        boardPanel.setBounds(
+                0,
+                scoreLabel.getY() + scoreLabel.getPreferredSize().height + 35,
+                CommonConstants.BOARD_SIZE.width,
+                CommonConstants.BOARD_SIZE.height
+        );
+
+        // create board
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                JButton button = new JButton();
+                button.setFont(new Font("Dialog", Font.PLAIN, 180));
+                button.setPreferredSize(CommonConstants.BUTTON_SIZE);
+                button.setBackground(CommonConstants.BACKGROUND_COLOR);
+                button.setBorder(BorderFactory.createLineBorder(CommonConstants.BOARD_COLOR));
+
+                board[i][j] = button;
+                boardPanel.add(board[i][j]);
+            }
+        }
+
+        // reset button
+        JButton resetButton = new JButton("Reset");
+        resetButton.setFont(new Font("Dialog", Font.PLAIN, 24));
+        resetButton.setBackground(CommonConstants.BOARD_COLOR);
+        resetButton.setBounds(
+                (CommonConstants.FRAME_SIZE.width - (-resetButton.getPreferredSize().width))/2,
+                CommonConstants.FRAME_SIZE.height - 100,
+                resetButton.getPreferredSize().width,
+                resetButton.getPreferredSize().height
+        );
+
+        getContentPane().add(turnLabel);
+        getContentPane().add(barLabel);
+        getContentPane().add(scoreLabel);
+        getContentPane().add(boardPanel);
+        getContentPane().add(resetButton);
     }
 }
